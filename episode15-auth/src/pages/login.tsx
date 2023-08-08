990c648523ac4d4ab61deeabe37daf2af2e024cf
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-08-08 16:52:19 +0300

    sign in with google

--- episode15-auth/src/pages/login.tsx
+++ episode15-auth/src/pages/login.tsx
@@ -1,10 +1,18 @@
-export const Login = () =>{
+import {auth,provider} from "../config/firebase";
+import {signInWithPopup} from "firebase/auth";
 
+
+
+export const Login = () =>{
+    const signWithGoogle = async () =>{
+        const result = await signInWithPopup(auth,provider);
+        console.log(result)
+    }
 
     return (
         <div>
             Sign In With Google To Continue
-            <button>Sign In with Google</button>
+            <button onClick={signWithGoogle}>Sign In with Google</button>
         </div>
     )
 }
