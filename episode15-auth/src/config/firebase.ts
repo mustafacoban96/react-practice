f3b44e8622ab4b5eaf0218818d2cf6a354fc5eb8
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-08-08 16:11:22 +0300

    firebase config is copied

---
+++ episode15-auth/src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDXp1z6grP7IFg3CD8gK4pBs6jt1wkkrkQ",
+  authDomain: "react-course-3b30d.firebaseapp.com",
+  projectId: "react-course-3b30d",
+  storageBucket: "react-course-3b30d.appspot.com",
+  messagingSenderId: "441619780072",
+  appId: "1:441619780072:web:344ebe006b29ff7c20945d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
