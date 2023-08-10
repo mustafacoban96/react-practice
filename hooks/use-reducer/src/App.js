e65d515cd19018e6352fffa367b204687ee09b40
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-08-10 22:44:42 +0300

    after reducer

--- hooks/use-reducer/src/App.js
+++ hooks/use-reducer/src/App.js
@@ -1,28 +1,43 @@
 import './App.css';
-import {useState} from 'react'
+import {useState,useReducer} from 'react';
+import { reducer } from './reducer';
+const initialState = {
+  data: "",
+  loading: false,
+  error: ""
+}
 
 function App() {
-  const [data,setData] = useState("");
-  const [loading,setLoading] = useState(false);
-  const [error, setError ] = useState("");
+  // if we have state more than one , we can use reducer
+  const [state, dispatch] = useReducer(reducer,initialState);
+  const {data,loading,error} = state;
+
+  
+  // const [data,setData] = useState("");
+  // const [loading,setLoading] = useState(false);
+  // const [error, setError ] = useState("");
 
 
   const fetchDog = () =>{
-    setLoading(true);
-    setError("");
-    setData("");
+    // // case fetch start
+    // setLoading(true);
+    // setError(""); 
+    // setData("");
 
+    dispatch({type: "FETCH_START"});
 
     fetch('https://dog.ceo/api/breeds/image/random')
     .then((res) =>res.json())
     .then((res) =>{
-      setLoading(false);
-      setData(res.message);
+      // setLoading(false);
+      // setData(res.message);
+      dispatch({type:"FETCH_SUCCESS" ,payload: res.message});
     })
     .catch(() =>{
-      setLoading(false);
-      setData("");
-      setError("Error fetching data");
+      // setLoading(false);
+      // setData("");
+      // setError("Error fetching data");
+      dispatch({type:"FETCH_ERROR", payload:"Error fetching data"});
     });
 
     
