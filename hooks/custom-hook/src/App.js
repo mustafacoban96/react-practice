1fd15d56f5ac194201a4851591322cec4f8fedb3
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-08-11 18:57:43 +0300

    after custom hook

--- hooks/custom-hook/src/App.js
+++ hooks/custom-hook/src/App.js
@@ -1,23 +1,39 @@
 import './App.css';
 import {useState} from 'react';
+import { useInput } from './hooks/useInput';
 
 function App() {
-  const [name,setName] = useState("");
-  const [age,setAge] = useState("");
-  const [email,setEmail] = useState("");
+
+  //Before custom hook1
+  // const [name,setName] = useState("");
+  // const [age,setAge] = useState("");
+  // const [email,setEmail] = useState("");
+
+
+  //Before custom hook2
+  // const [inputs, setInputs] = useState({name: '',age: '',email: ''});
+
+  // const handleChange = event =>{
+  //   setInputs({...inputs, [event.target.name]: event.target.value})
+  // }
+
+  // after custom hook
+  const [inputs,setInputs] = useInput({name: '',age: '',email: ''});
+
+
   return (
     <div className="App">
       <h1>React Course</h1>
       <h2>How to do custom-hook</h2>
       <div className='form'>
         <label htmlFor='name'>
-            Name: <input name='name' value={name} onChange={e =>setName(e.target.value)}/>
+            Name: <input name='name' value={inputs.name} onChange={setInputs}/>
         </label>
         <label htmlFor='age'>
-            Age: <input name='age' value={age} onChange={e =>setAge(e.target.value)}/>
+            Age: <input name='age' value={inputs.age} onChange={setInputs}/>
         </label>
         <label htmlFor='email'>
-            Email: <input name='email' value={email} onChange={e =>setEmail(e.target.value)}/>
+            Email: <input name='email' value={inputs.email} onChange={setInputs}/>
         </label>
       </div>
     </div>
