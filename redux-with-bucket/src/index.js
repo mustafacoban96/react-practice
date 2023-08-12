b39985b1291a4c4fbdf3aeb15c17bf79fd377e45
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-08-12 11:26:35 +0300

    connect redux

--- redux-with-bucket/src/index.js
+++ redux-with-bucket/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+// we import createStore for connection myApp to redux ->1
+import { createStoreHook } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
