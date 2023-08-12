29d2830c85a650a413ef7340fdea5b5d56b346c2
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-08-12 14:52:57 +0300

    we connect our app to redux

--- redux-with-bucket/src/index.js
+++ redux-with-bucket/src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 // we import createStore for connection myApp to redux ->1
-import { createStoreHook } from 'react-redux';
+import { createStore } from 'redux';
+// ->3 
+import {reducer} from './reducers/index'
+//->4
+import { Provider } from 'react-redux';
+
+//  state is managed by one global store.->2
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  //we wrap app witn provider to all of components access the state. ->4
+  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Provider>
 );
 
