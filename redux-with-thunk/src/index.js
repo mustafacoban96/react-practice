9bbae88385fbfa4350ff6efafffde3c116622481
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-08-23 14:03:14 +0300

    reducer temp is designed

--- redux-with-thunk/src/index.js
+++ redux-with-thunk/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {createStore} from 'redux';
+import {reducer} from './reducers/index'
+import { Provider } from 'react-redux';
+const store = createStore(reducer);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
+  <Provider store={store}>
     <App />
+  </Provider>
+    
   // </React.StrictMode>
 );
 
---
+++ redux-with-thunk/src/reducers/index.js
@@ -0,0 +1,13 @@
+
+
+
+const INITIAL_STATE = {
+    countries: []
+}
+
+
+
+export const reducer = (state,action) =>{
+
+    return state
+}
